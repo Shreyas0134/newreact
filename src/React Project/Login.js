4d9d4f4990c086528305e1fc000e1a738fa0ec72
Shreyas0134 <shreyaskarandikar2000@gmail.com>
2022-12-30 19:23:53 +0530

    changes

--- src/React Project/Login.js
+++ src/React Project/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './project.css'
+import './login.css'
 import photo from "./Tablet login-amico.svg";
 
 
--- src/React Project/MainPage.js
+++ src/React Project/MainPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './MainPage.css'
 
 const MainPage = () => {
   return (
    <div>
+    njlgnsg
     
 
 
